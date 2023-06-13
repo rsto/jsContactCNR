7094007ecb7a370ce5ab6e97057fd479cdbfb8e7
Robert Stepanek <rsto@paranoia.at>
2023-06-13 08:07:10 +0200

    Convert vcard and jscontact

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,40 +2,71 @@ package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpServer;
+import it.cnr.iit.jscontact.tools.dto.Card;
 import it.cnr.iit.jscontact.tools.exceptions.CardException;
+import it.cnr.iit.jscontact.tools.vcard.converters.config.JSContact2VCardConfig;
 import it.cnr.iit.jscontact.tools.vcard.converters.config.VCard2JSContactConfig;
+import it.cnr.iit.jscontact.tools.vcard.converters.jscontact2vcard.JSContact2VCard;
 import it.cnr.iit.jscontact.tools.vcard.converters.vcard2jscontact.VCard2JSContact;
 
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 
-import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
-import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.HttpURLConnection.*;
 
 public class Main {
-    private static final VCard2JSContact vCard2JSContact = VCard2JSContact.builder().config(VCard2JSContactConfig.builder().build()).build();
+    private static final VCard2JSContact vCard2JSContact = VCard2JSContact.builder()
+            .config(VCard2JSContactConfig.builder().build())
+            .build();
+    private static final JSContact2VCard jsContact2vCard = JSContact2VCard.builder()
+            .config(JSContact2VCardConfig.builder().build())
+            .build();
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress("localhost", 8080), 0);
 
         server.createContext("/convert", exchange -> {
-            try {
-                var body = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
-                var cards = vCard2JSContact.convert(body);
-                exchange.getResponseHeaders().add("Content-Type", "application/json");
-                exchange.sendResponseHeaders(HTTP_OK, 0);
-                exchange.getResponseBody().write(mapper
-                        .writerWithDefaultPrettyPrinter()
-                        .writeValueAsString(cards)
-                        .getBytes(StandardCharsets.UTF_8));
-                 exchange.getResponseBody().close();
-            } catch (CardException e) {
-                exchange.getResponseHeaders().add("Content-Type", "application/json");
-                exchange.sendResponseHeaders(HTTP_BAD_REQUEST, 0);
-                exchange.getResponseBody().write(mapper.writeValueAsString(e.getMessage()).getBytes(StandardCharsets.UTF_8));
+
+            if (!"POST".equals(exchange.getRequestMethod())) {
+                exchange.getResponseHeaders().add("Allow", "POST");
+                exchange.sendResponseHeaders(HTTP_BAD_METHOD, 0);
                 exchange.getResponseBody().close();
+                return;
             }
+
+            var reqBody = new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
+            var reqCType = exchange.getRequestHeaders().getFirst("Content-Type");
+            String resCType;
+            String resBody;
+            int statusCode;
+
+            try {
+                if ("text/vcard".equals(reqCType)) {
+                    var cards = vCard2JSContact.convert(reqBody);
+                    resCType = "application/jscontact+json";
+                    resBody = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(cards);
+                    statusCode = HTTP_OK;
+                } else if ("application/jscontact+json".equals(reqCType)) {
+                    var cards = Card.toJSCards(reqBody);
+                    resCType = "text/vcard";
+                    resBody = jsContact2vCard.convertToText(cards);
+                    statusCode = HTTP_OK;
+                } else {
+                    statusCode = HTTP_UNSUPPORTED_TYPE;
+                    resCType = "application/jscontact+json";
+                    resBody = "Must be text/vcard or application/jscontact+json";
+                }
+            } catch (CardException e) {
+                statusCode = HTTP_BAD_REQUEST;
+                resCType = "text/plain";
+                resBody = e.getMessage();
+            }
+
+            exchange.getResponseHeaders().add("Content-Type", resCType);
+            exchange.sendResponseHeaders(statusCode, 0);
+            exchange.getResponseBody().write(resBody.getBytes(StandardCharsets.UTF_8));
+            exchange.getResponseBody().close();
         });
 
         server.start();
